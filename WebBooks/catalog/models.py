07bced2c294244b95b9e2a56bc113abaf60a2531
cube-2025 <piyanzin.nik2004@gmail.com>
2025-09-09 16:35:53 +0300

    Начали проект WebBook

---
+++ WebBooks/catalog/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.urls import reverse
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200,
+                            help_text='Введите жанр книги',
+                            varbose_name='Жанр книги')
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=20,
+                            help_text='Введите язык книги',
+                            verbose_name='Язык книги')
+    def __str__(self):
+        return self.name
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=20,
+                            help_text='Введите наименование издательства',
+                            verbose_name='Издательство')
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100,
+                                  help_text='Введите имя автора',
+                                  verbose_name='Имя автора')
+    last_name = models.CharField(max_length=100,
+                                  help_text='Введите фамилию автора',
+                                  verbose_name='Фамилию автора')
+    date_of_birth = models.DateField(
+        help_text='Введите дату рождения',
+        verbose_name='Дата рождения',
+        null=True, blank=True
+    )
+    about = models.TextField(help_text='Введите сведения об авторе',
+                             verbose_name='Сведения об авторе')
+    photo = models.ImageField(upload_to='images',
+                              help_text='Введите фото автора',
+                              null=True, blank=True)
+    def __str__(self):
+        return self.last_name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200,
+                             help_text='Введите название книги',
+                             verbose_name='Название книги')
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE,
+                              help_text='Выберите жанр книги',
+                              verbose_name='Жанр книги',
+                              null=True)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                              help_text='Выберите язык книги',
+                              verbose_name='Язык книги',
+                              null=True)
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE,
+                              help_text='Выберите издательство книги',
+                              verbose_name='Издательство книги',
+                              null=True)
+    year = models.CharField(max_length=4,
+                            help_text='Введите год издания',
+                            verbose_name='Год издания')
+    author = models.ManyToManyField('Author',
+                                    help_text='Выберите автора',
+                                    verbose_name='Автор книги')
+    isbn = models.CharField(max_length=13,
+                            help_text='Должно содержать 13 символов',
+                            verbose_name='ISBN книги')
