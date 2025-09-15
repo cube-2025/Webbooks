3d1a6a1ce95053d17ddde48d1459689f8384b550
cube-2025 <piyanzin.nik2004@gmail.com>
2025-09-15 10:09:13 +0300

    шаблоны

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+from django.shortcuts import render
+
 def index(request):
-    return HttpResponse('Главная страница магазин Книжный, что пишем, то и пишем')
+    text_head = 'Книжный магазин'
+    text_body = 'Это содержимое главной страницы сайта'
+    context = {
+        'text_head': text_head,
+        'text_body': text_body
+    }
+    return render(request, 'catalog/index.html', context)
